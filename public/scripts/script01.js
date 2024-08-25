8ec4b69558c0861925acbc390ba973692cceca41
aker moh <akerm054@gmail.com>
2024-08-25 16:53:17 +0200

    Fix directory structure

--- public/scripts/script01.js
+++ public/scripts/script01.js
@@ -1,14 +1,23 @@
-document.addEventListener('DOMContentLoaded', async () => {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNTDHR67L48F1nPReRs2dSoQ-PxgNKWYM",
+  authDomain: "login2-d485e.firebaseapp.com",
+  projectId: "login2-d485e",
+  storageBucket: "login2-d485e.appspot.com",
+  messagingSenderId: "602998933832",
+  appId: "1:602998933832:web:a397944522901f3c12cb7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.addEventListener('DOMContentLoaded', () => {
   const profileCircle = document.getElementById('profileCircle');
   const signInButton = document.getElementById('signbtn');
-
-  // Fetch Firebase config from server
-  const response = await fetch('/getFirebaseConfig');
-  const firebaseConfig = await response.json();
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
 
   // Firebase auth state change
   onAuthStateChanged(auth, (user) => {
